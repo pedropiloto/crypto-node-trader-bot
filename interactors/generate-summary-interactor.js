555a190d3279fa8786aab76b6f33cba5120e577a
Pedro Piloto <pedro@pedropiloto.com>
2020-11-25 03:02:48 +0000

    summary

--- interactors/generate-summary-interactor.js
+++ interactors/generate-summary-interactor.js
@@ -14,67 +14,75 @@ class GenerateSummaryInteractor {
     const result = {};
     const enrichedTrades = [];
 
-    const completeTrades = await TradeModel.find({ status: COMPLETED });
-    const incompleteTrades = await TradeModel.find({ status: STARTED });
+    const trades = await TradeModel.find({});
 
-    for (let i = 0; i < completeTrades.length; i++) {
-      const buyFills = await
-      this.coinbaseGateway.getFillsByOrderId(completeTrades[i].buy_order_id);
-      const sellFills = await
-      this.coinbaseGateway.getFillsByOrderId(completeTrades[i].sell_order_id);
-      enrichedTrades.push(
-        Object.assign(completeTrades[i], { buy_fills: buyFills, sell_fills: sellFills }),
-      );
+    const tradesSymbol = trades.filter((x) => x.symbol);
+
+    for (let x = 0; x < tradesSymbol.length; x++) {
+      const symbol = tradesSymbol[x];
+
+      const completeTrades = await TradeModel.find({ symbol, status: COMPLETED });
+      const incompleteTrades = await TradeModel.find({ symbol, status: STARTED });
+
+      for (let i = 0; i < completeTrades.length; i++) {
+        const buyFills = await
+        this.coinbaseGateway.getFillsByOrderId(completeTrades[i].buy_order_id);
+        const sellFills = await
+        this.coinbaseGateway.getFillsByOrderId(completeTrades[i].sell_order_id);
+        enrichedTrades.push(
+          Object.assign(completeTrades[i], { buy_fills: buyFills, sell_fills: sellFills }),
+        );
+      }
+
+      enrichedTrades.forEach((trade) => {
+        const totalBuy = trade.buy_fills.reduce((accumulator, current) => {
+          const priceBig = new Big(current.price);
+          const sizeBig = new Big(current.size);
+          const feeBig = new Big(current.fee);
+          const totalCost = priceBig.mul(sizeBig).add(feeBig);
+          return new Big(accumulator).add(totalCost);
+        }, 0);
+
+        const totalSell = trade.sell_fills.reduce((accumulator, current) => {
+          const priceBig = new Big(current.price);
+          const sizeBig = new Big(current.size);
+          const feeBig = new Big(current.fee);
+          const totalCost = priceBig.mul(sizeBig).sub(feeBig);
+          return new Big(accumulator).add(totalCost);
+        }, 0);
+
+        if (!result[symbol]) result[symbol] = {};
+        // Total Buy
+        result[symbol].total_buy
+          ? result[symbol].total_buy = (
+            (new Big(result[symbol].total_buy)).add((new Big(totalBuy)))
+          ).toString()
+          : result[symbol].total_buy = totalBuy.toString();
+        // Total Sell
+        result[symbol].total_sell
+          ? result[symbol].total_sell = ((
+            new Big(result[symbol].total_sell)).add((new Big(totalSell)))
+          ).toString()
+          : result[symbol].total_sell = totalSell.toString();
+        // Total Profit
+        result[symbol].total_profit = (new Big(result[symbol].total_sell))
+          .sub((new Big(result[symbol].total_buy))).toString();
+        // Total Buy Fills
+        result[symbol].total_buy_fills
+        // eslint-disable-next-line no-plusplus
+          ? result[symbol].total_buy_fills++
+          : result[symbol].total_buy_fills = 1;
+        // Total Sell Fiills
+        result[symbol].total_sell_fills
+        // eslint-disable-next-line no-plusplus
+          ? result[symbol].total_sell_fills++
+          : result[symbol].total_sell_fills = 1;
+        // Total Complete Trades
+        result[symbol].complete_trades = enrichedTrades.length;
+        // Total Incomplete Trades
+        result[symbol].incomplete_trades = incompleteTrades.length;
+      });
     }
-
-    enrichedTrades.forEach((trade) => {
-      const totalBuy = trade.buy_fills.reduce((accumulator, current) => {
-        const priceBig = new Big(current.price);
-        const sizeBig = new Big(current.size);
-        const feeBig = new Big(current.fee);
-        const totalCost = priceBig.mul(sizeBig).add(feeBig);
-        return new Big(accumulator).add(totalCost);
-      }, 0);
-
-      const totalSell = trade.sell_fills.reduce((accumulator, current) => {
-        const priceBig = new Big(current.price);
-        const sizeBig = new Big(current.size);
-        const feeBig = new Big(current.fee);
-        const totalCost = priceBig.mul(sizeBig).sub(feeBig);
-        return new Big(accumulator).add(totalCost);
-      }, 0);
-
-      if (!result[trade.symbol]) result[trade.symbol] = {};
-      // Total Buy
-      result[trade.symbol].total_buy
-        ? result[trade.symbol].total_buy = (
-          (new Big(result[trade.symbol].total_buy)).add((new Big(totalBuy)))
-        ).toString()
-        : result[trade.symbol].total_buy = totalBuy.toString();
-      // Total Sell
-      result[trade.symbol].total_sell
-        ? result[trade.symbol].total_sell = ((
-          new Big(result[trade.symbol].total_sell)).add((new Big(totalSell)))
-        ).toString()
-        : result[trade.symbol].total_sell = totalSell.toString();
-      // Total Profit
-      result[trade.symbol].total_profit = (new Big(result[trade.symbol].total_sell))
-        .sub((new Big(result[trade.symbol].total_buy))).toString();
-      // Total Buy Fills
-      result[trade.symbol].total_buy_fills
-        // eslint-disable-next-line no-plusplus
-        ? result[trade.symbol].total_buy_fills++
-        : result[trade.symbol].total_buy_fills = 1;
-      // Total Sell Fiills
-      result[trade.symbol].total_sell_fills
-        // eslint-disable-next-line no-plusplus
-        ? result[trade.symbol].total_sell_fills++
-        : result[trade.symbol].total_sell_fills = 1;
-      // Total Complete Trades
-      result[trade.symbol].complete_trades = enrichedTrades.length;
-      // Total Incomplete Trades
-      result[trade.symbol].incomplete_trades = incompleteTrades.length;
-    });
     return result;
   }
 }
